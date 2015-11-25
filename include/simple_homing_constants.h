752e38f05f2bde12505d0e02f6989d240a8c9c62
Enrico Mingo <enrico.mingo@iit.it>
2015-11-25 12:49:56 +0100

    Set header for bigman

--- include/simple_homing_constants.h
+++ include/simple_homing_constants.h
@@ -4,13 +4,13 @@
 // param helper const
 // torso
 #define PARAM_ID_TORSO 0
-#define PARAM_SIZE_TORSO 2
+#define PARAM_SIZE_TORSO 3
 // left_arm
 #define PARAM_ID_LEFT_ARM 1
-#define PARAM_SIZE_LEFT_ARM 8
+#define PARAM_SIZE_LEFT_ARM 7
 // right_arm
 #define PARAM_ID_RIGHT_ARM 2
-#define PARAM_SIZE_RIGHT_ARM 8
+#define PARAM_SIZE_RIGHT_ARM 7
 // left_leg
 #define PARAM_ID_LEFT_LEG 3
 #define PARAM_SIZE_LEFT_LEG 6
